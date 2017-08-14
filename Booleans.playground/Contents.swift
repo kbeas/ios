1de60cdb1c94416c3a853b7b49328b15933d22a4
Karlo Beas <karlo.beas@caliente.com.mx>
2017-08-14 16:01:34 -0700

    Boolean, String, Char

---
+++ Booleans.playground/Contents.swift
@@ -0,0 +1,28 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//Bool
+
+var positive:Bool = true
+var negative:Bool = false
+
+//Operadores de asignacion ==
+
+var x = 10
+var y = 10
+
+var equal = x == y
+
+//String
+
+let alfa:String = "ABCDEFGH"
+let beto:String = "IJKLMNOPQRSTUVWXYZ"
+
+let alfabeto = alfa + beto
+
+//Char
+
+let char0:Character = "x"
+let char1:Character = "y"
+
