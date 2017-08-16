3132f6d753f76ee604182b7ca957aaa33d16f6d0
Karlo Beas <karlo.beas@caliente.com.mx>
2017-08-16 09:51:52 -0700

    App Audio

---
+++ Clase 3/AppSwitchAudio/AppSwitchAudio/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  AppSwitchAudio
+//
+//  Created by Karlo Beas on 8/16/17.
+//  Copyright © 2017 Karlo Beas. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+
+    var player: AVAudioPlayer!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        guard let asset = NSDataAsset(name: "BAD TO THE BONE") else { print("Error Loading Audio.");
+            return }
+    
+        do {
+            player = try AVAudioPlayer(data: asset.data)
+            player.volume = 0
+            player.numberOfLoops = -1
+            player.play()
+        } catch { print("Error playing audio"); return }
+    }
+    
+    @IBAction func onSwitchChanged(_ sender: UISwitch) {
+        if sender.isOn {
+            player.setVolume(1, fadeDuration: 2)
+        } else {
+            player.setVolume(0, fadeDuration: 2)
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
