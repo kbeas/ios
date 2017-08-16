ad3c722cee124027a3bb797223762b846a9ded85
Karlo Beas <karlo.beas@caliente.com.mx>
2017-08-16 13:33:20 -0700

    MapKit

---
+++ Clase 3/MKV/MKV/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  MKV
+//
+//  Created by Karlo Beas on 8/16/17.
+//  Copyright © 2017 Karlo Beas. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var mapView: MKMapView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let location = CLLocationCoordinate2D(latitude: 35.687571, longitude: 139.799074)
+        let span = MKCoordinateSpanMake(0.05, 0.05)
+        let region = MKCoordinateRegion(center: location, span: span)
+        mapView.setRegion(region, animated: true)
+        
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = location
+        annotation.title = "Japan"
+        annotation.subtitle = "Tokyo"
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
